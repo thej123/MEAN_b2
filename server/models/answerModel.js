fa7f9d0a877690222945c7c8c60b8c7e53245158
Thej <tkodiyat01@gmail.com>
2017-10-25 18:56:03 -0700

    fixed syntac error on models and html

--- server/models/answerModel.js
+++ server/models/answerModel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 var AnswerSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    answer: {type: String, required: true},
+    answer: {type: String, required: true, minlength: "5"},
     details: {type: String},
     likes: {type: Number, required: true, default: 0},
     _questionID: {
